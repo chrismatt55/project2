1c854ff3f667d99e06b3a6250d181d13c67228a1
AlexisYi <37344325+AlexisYi@users.noreply.github.com>
2018-04-03 13:14:02 -0400

    Invoice page updated
    updated from Blog

---
+++ private/classes/invoice.class.php
@@ -0,0 +1,31 @@
+<?php
+
+class Invoice extends DatabaseObject {
+
+static protected $table_name = 'invoice';
+static protected $db_columns = ['invoiceid', 'recieveDate', 'manufacturer', 'referenceNumber', 'amount'];
+
+public $invoiceid;
+public $recieveDate;
+public $manufacturer;
+public $referenceNumber;
+public $amount;
+
+
+// construct method
+public function __construct($args=[]) {
+  $this->invoiceid = $args['invoiceid'] ?? NULL;
+  $this->recieveDate = $args['recieveDate'] ?? '';
+  $this->manufacturer = $args['manufacturer'] ?? '';
+  $this->referenceNumber = $args['referenceNumber'] ?? '';
+  $this->amount = $args['Amount'] ?? '';
+ 
+  
+}
+
+
+
+
+
+
+?>
