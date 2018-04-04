35ebaae4333cece3b27c9552a5b5ae0d536ecf8b
chrismatt55 <37344314+chrismatt55@users.noreply.github.com>
2018-04-04 17:18:42 -0400

    DB update
    DB update

--- private/db_config.php
+++ private/db_config.php
@@ -3,7 +3,7 @@
 
 define("DB_SERVER", "localhost");
 define("DB_USER", "chrism86_pro2");
-define("DB_PASS", "yu019QgTv3");
+define("DB_PASS", "HMKp2l9A3o");
 define("DB_NAME", "chrism86_pro2");
 
 ?>
