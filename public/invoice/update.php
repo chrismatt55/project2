715b6b0e7569fa103a4fe128010848e5fdbf9568
chrismatt55 <37344314+chrismatt55@users.noreply.github.com>
2018-04-04 18:31:03 -0400

    invoice
    invoice

--- public/invoice/update.php
+++ public/invoice/update.php
@@ -3,91 +3,55 @@
 
 require_once("../../private/initialize.php");
 require_login();
-$page_title = "Update Blogs";
+$page_title = "Update Invoices";
 $current = "update";
 include(SHARED_PATH . '/public_header.php');
 
 //get the persons id - id
-$blogid = $_GET['blogid'] ?? false;
+$invoiceid = $_GET['invoiceid'] ?? false;
 
 // find the user info based on passed id
-$blog = Blog::find_by_id($blogid);
+$invoice = Invoice::find_by_id($invoiceid);
 
 // set new variables to
 
 
-$blogName = $blog->blogName;
-$url = $blog->url;
-$email= $blog->email;
-$contactFName = $blog->contactFName;
-$contactLName = $blog->contactLName;
-$qualityScore = $blog->qualityScore;
-$mozDA = $blog->mozDA;
-$sponsors = $blog->sponsors;
-$fqshop = $blog->fqshop;
-$gfairy = $blog->gfairy;
-$mstar = $blog->mstar;
+$receiveDate = $invoice->receiveDate;
+$manufacturer = $invoice->manufacturer;
+$referenceNum = $invoice->referenceNum;
+$amt = $invoice->amt;
 
-//
-if($fqshop == 0){
-  $fq = "unchecked";
-} else {
-  $fq = "checked";
-}
-//
-if($gfairy == 0){
-  $gf = "unchecked";
-} else {
-  $gf = "checked";
-}
 
-//
-if($mstar == 0){
-  $ms = "unchecked";
-} else {
-  $gf = "checked";
 
-}
+
 
 
 
 if(is_post_request()) {
     // get post variables
 
-    $blogid = $_POST['blogid'];
-    $blogName = $_POST['blogName'];
-    $url = $_POST['url'];
-    $email = $_POST['email'];
-    $contactFName = $_POST['contactFName'];
-    $contactLName = $_POST['contactLName'];
-    $qualityScore = $_POST['qualityScore'];
-    $mozDA = $_POST['mozDA'];
-    $sponsors = $_POST['sponsors'];
-    $fqshop = $_POST['fqshop'];
-    $gfairy = $_POST['gfairy'];
-    $mstar = $_POST['mstar'];
+    $invoiceid = $_POST['invoiceid'];
+    $receiveDate = $_POST['receiveDate'];
+    $manufacturer = $_POST['manufacturer'];
+    $referenceNum = $_POST['referenceNum'];
+    $amt = $_POST['amt'];
 
 
 
   //create an array called args to be used with __construct
     $args = [];
     $args['id'] = $id;
-    $args['blogName'] = $blogName;
-    $args['url'] = $url;
-    $args['email'] = $email;
-    $args['contactFName'] = $contactFName;
-    $args['contactLName'] = $contactLName;
-    $args['qualityScore'] = $qualityScore;
-    $args['mozDA'] = $mozDA;
-    $args['sponsors'] = $sponsors;
-    $args['fqshop'] = $fqshop;
-    $args['mstar'] = $mstar;
+    $args['receiveDate'] = $receiveDate;
+    $args['manufacturer'] = $manufacturer;
+    $args['referenceNum'] = $referenceNum;
+    $args['amt'] = $amt;
+
 
 
     //instantiate a new object and use the merge attributes and save to update.
-    $blog = new Blog;
-    $blog->merge_attributes($args);
-    $blog->update($blogid);
+    $invoice = new Invoice;
+    $invoice->merge_attributes($args);
+    $invoice->update($invoiceid);
 
     //after saving redirect back to home page.
     header('Location: index.php');
@@ -100,26 +64,14 @@ if(is_post_request()) {
       <div class="container">
           <form action="update.php" method="post">
             <fieldset>
-              <legend>Updated Blog Information</legend>
-              <input name="blogid" type="hidden" value="<?php echo $blogid;?>">
-              <p>Blog Name: <input type="text" name="blogName" size="40" va;ue="<?php echo $blogName; ?>"></p>
-              <p>URL: <input type="text" name="url" size="40" value="<?php echo $url; ?>"></p>
-              <p>Email: <input type="text" name="email" size="40" value="<?php echo $email; ?>"></p>
-              <p>Contact First Name: <input type="text" name="contactFName" size="40" value="<?php echo $contactFName; ?>"></p>
-              <p>Contact Last Name: <input type="text" name="contactLName" value="<?php echo $contactLName; ?>"></p>
-              <p>Quality Score: <input type="number" name="qualityScore" value="<?php echo $qualityScore; ?>" readonly></p>
+              <legend>Updated Invoice Information</legend>
+              <input name="invoiceid" type="hidden" value="<?php echo $invoiceid;?>">
+              <p>Reference Number: <input type="number" name="referenceNum" value="<?php echo $referenceNum; ?>"></p>
+              <p>Manufacturer Name: <input type="text" name="manufacturer" size="40" value="<?php echo $manufacturer; ?>"></p>
+              <p>Amount: <input type="number" name="amt"  value="<?php echo $amt; ?>"></p>
+              <p>Receive Date: <input type="date" name="receiveDate" value="<?php echo $receiveDate; ?>"></p>
 
-              <p>Moz Domain Authority: <input type="text" name="mozDA" value=" <?php echo $mozDA; ?>"></p>
-              <p># of Spnsors: <input type="text" name="sponsors" value=" <?php echo $sponsors; ?>"></p>
 
-            <input type="hidden" name="fqshop" value=0>;
-            <p>Fat Quarter Shop: <input type="checkbox" name="fqshop" value="<?php echo $fqshop; ?>"></p>
-
-            <input type="hidden" name="gfairy" value=0>;
-            <p>Green Fairy: <input type="checkbox" name="gfairy" value="<?php echo $gfairy; ?>"></p>
-
-            <input type="hidden" name="mstar" value=0>;
-            <p>Missouri Star Shop: <input type="checkbox" name="mstar" value="<?php echo $mstar; ?>"></p>
 
             <button type="submit" value="submit">Update</button>
             <button type="button" onclick="location='index.php'">Cancel Update</button>
