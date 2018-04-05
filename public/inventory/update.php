89589e4e72675c88702455dbfd408a7df6aedb59
chrismatt55 <37344314+chrismatt55@users.noreply.github.com>
2018-04-05 12:43:52 -0400

    Inventory
    Inventory

--- public/inventory/update.php
+++ public/inventory/update.php
@@ -11,43 +11,37 @@ include(SHARED_PATH . '/public_header.php');
 $inventoryid = $_GET['inventoryid'] ?? false;
 
 // find the user info based on passed id
-$inventory = Inventory::find_by_id($contractid);
+$inventory = Inventory::find_by_id($inventoryid);
 
 // set new variables to
-$paymentDate = $contract->paymentDate;
-$paymentAmt = $contract->paymentAmt;
-$contractLength = $contract->contractLength;
-$blogid = $contract->blogid;
+$inventoryamt = $inventory->inventoryamt;
+
 
 
 
 
 if(is_post_request()) {
     // get post variables
-    $contractid = $_POST['contractid'];
-    $blogid = $_POST['blogid'];
-    $paymentDate = $_POST['paymentDate'];
-    $paymentAmt = $_POST['paymentAmt'];
-    $contractLength = $_POST['contractLength'];
+    $inventoryid = $_POST['inventoryid'];
+    $inventoryamt = $_POST['inventoryamt'];
+
 
   //create an array called args to be used with __construct
     $args = [];
-    $args['contractid'] = $contractid;
-    $args['paymentDate'] = $paymentDate;
-    $args['paymentAmt'] = $paymentAmt;
-    $args['contractLength'] = $contractLength;
-    $args['blogid'] = $blogid;
+    $args['inventoryid'] = $inventoryid;
+    $args['inventoryamt'] = $inventoryamt;
+
 
 
 
 
     //instantiate a new object and use the merge attributes and save to update.
-    $contract = new Contract;
-    $contract->merge_attributes($args);
-    $contract->update($contractid);
+    $inventory = new Inventory;
+    $inventory->merge_attributes($args);
+    $inventory->update($inventoryid);
 
     //after saving redirect back to home page.
-    header("Location: index.php?blogid=$blogid");
+    header("Location: index.php?inventoryid=$inventoryid");
 
 
 }
@@ -58,14 +52,11 @@ if(is_post_request()) {
       <div class="container">
           <form action="update.php" method="post">
             <fieldset>
-              <legend>Updated Contact Information</legend>
-              <input name="contractid" type="hidden" value="<?php echo $contractid;?>">
-              <input name="blogid" type="hidden" value="<?php echo $blogid;?>">
-              <p>Contract Date: <input type="date" name="paymentDate" value="<?php echo $paymentDate; ?>"></p>
-              <p>Payment Amount: <input type="text" name="paymentAmt" value="<?php echo $paymentAmt; ?>"></p>
-              <p>First Contract Length in Months: <input type="number" name="contractLength" min ="1" value="<?php echo $firstName; ?>"></p>
+              <legend>Updated Inventory Information</legend>
+              <input name="inventoryid" type="hidden" value="<?php echo $inventoryid;?>">
+              <p>Inventory Amount: <input type="number" name="inventoryamt" min ="1" value="<?php echo $inventoryamt; ?>"></p>
               <button type="submit" value="Submit">Submit</button>
-              <button type="button" onclick="location='index.php?blogid=<?php echo $blogid; ?>'">Cancel Update</button>
+              <button type="button" onclick="location='index.php?inventoryid=<?php echo $inventoryid; ?>'">Cancel Update</button>
             </fieldset>
           </form>
 
