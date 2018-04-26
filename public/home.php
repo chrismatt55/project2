05d0c397b227deb7ba319e3b4d2bab59e0e30f30
chrismatt55 <37344314+chrismatt55@users.noreply.github.com>
2018-04-26 13:30:12 -0400

    inventory
    inventory

--- public/home.php
+++ public/home.php
@@ -14,7 +14,7 @@ include(SHARED_PATH . '/public_header.php');
      <br>
 
      <h2>Welcome to the Invoice and COGS Manager</h2>
-     <br><?php echo "We are trying to keep our inventory at " . $inventory->inventoryamt; ?><br>
+     <br>We are trying to keep our inventory at $300,000.00<br>
      <br>You have $0.00 left to spend this month<br>
 
 
